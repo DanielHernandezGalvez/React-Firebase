98829b94a1e654dceb40c0571053944253b888d0
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-01-23 17:49:00 -0600

    Create detalles.jsx

---
+++ detalles.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Detalles({ children, estado, cambiarEstado }) {
+  return (
+    <>
+      {estado && (
+        <div className='detalleOverlay'>
+          <div className='contenedorModal'>
+            
+            <button
+              className='botonCerrarModal'
+              onClick={() => cambiarEstado(false)}
+            >
+              X
+            </button>
+            {children}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
