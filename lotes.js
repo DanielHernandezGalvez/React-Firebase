f719d1aa7a64bf696d32736297a3e07f06861dd0
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-01-23 17:49:20 -0600

    Update lotes.js

--- lotes.js
+++ lotes.js
@@ -1,19 +1,21 @@
-import HeaderND from "../components/headerND";
-import NavHome from "../components/navHome";
-import styles from "../styles/lotes.module.css";
-import { useState, useEffect } from "react";
+import HeaderND from "../../components/headerND";
+import NavHome from "../../components/navHome";
+import styles from "../../styles/lotes.module.css";
+import { useState } from "react";
+import Detalle from "../../components/detalle";
 
-const lotes = () => {
-  const response = require("./api/fakeApiLotes.json");
-
+const Lotes = () => {
+  const response = require("../api/fakeApiLotes.json");
   const data = response.data;
+  const [estadoModal, setEstadoModal] = useState(
+    Array(data.length).fill(false)
+  );
 
   data.sort(function (a, b) {
     var dateA = new Date(a.lotefecha),
       dateB = new Date(b.lotefecha);
     return dateB - dateA;
   });
-  console.log(data);
 
   return (
     <>
@@ -21,23 +23,74 @@ const lotes = () => {
       <NavHome />
       <div className={styles.bgImg}></div>
       <h1 className={styles.titulo}>Lotes</h1>
+      <hr />
       {data.map((producto, index) => {
         return (
-          <div className='container' key={index}>
-            <hr />
-            <h5>{producto.lotefecha}</h5>
-            <h2 className={styles.title}>{producto.lotedescripcion}</h2>
-            {producto.loteactivo === true ? (
-              <h3 className={styles.activo}>Activo</h3>
-            ) : (
-              <h3 className={styles.inactivo}>Inactivo</h3>
-            )}
+          <div key={index}>
+            <div className='contenedorLista'>
+              <button
+                className='botonloteLista'
+                onClick={() => {
+                  let newEstadoModal = [...estadoModal];
+                  newEstadoModal[index] = !newEstadoModal[index];
+                  setEstadoModal(newEstadoModal);
+                }}
+              >
+                <h1>{producto.lotedescripcion}</h1>
+                <h3>{producto.lotefecha}</h3>
+              </button>
+            </div>
+            <Detalle
+              estado={estadoModal[index]}
+              cambiarEstado={() => {
+                let newEstadoModal = [...estadoModal];
+                newEstadoModal[index] = !newEstadoModal[index];
+                setEstadoModal(newEstadoModal);
+              }}
+              producto={producto}
+            >
+              <div className='contenidoModal'>
+                <h1 className={styles.title}>
+                  Lote: {producto.lotedescripcion}
+                </h1>
+                <h5>fecha: {producto.lotefecha}</h5>
+
+                {producto.loteactivo === true ? (
+                  <h2 className={styles.activo}>Activo</h2>
+                ) : (
+                  <h2 className={styles.inactivo}>Inactivo</h2>
+                )}
+
+                <p>Feca de inicio: {producto.lotefechainicio}</p>
+                <p>Feca de cierre: {producto.lotefechhacierre}</p>
+
+                <h3 className={styles.title}>Productos:</h3>
+                {producto.productos.map((producto, index) => {
+                  return (
+                    <>
+                      <div key={index}>
+                        <hr />
+                        <p>Clave: {producto.productoclave}</p>
+                        <p>{producto.productoimagen}</p>
+                        <p>Cantidad: {producto.productocantidad}</p>
+                        <p>Descripción: {producto.productodescripcion}</p>
+                      </div>
+                    </>
+                  );
+                })}
+              </div>
+            </Detalle>
           </div>
         );
       })}
-      <div className={styles.footer}></div>
+      <div className='pb-5 mb-5'>
+        <div className='pb-5 mb-5'>
+          <div className='pb-5 mb-5'></div>
+        </div>
+      </div>
     </>
   );
 };
 
-export default lotes;
+export default Lotes;
+
