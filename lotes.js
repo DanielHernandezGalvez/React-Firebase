68e3b145128e411e62a0232cf69d00d452dc09be
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-01-26 08:54:00 -0600

    Update lotes.js

--- lotes.js
+++ lotes.js
@@ -3,33 +3,57 @@ import NavHome from "../../components/navHome";
 import styles from "../../styles/lotes.module.css";
 import { useState } from "react";
 import Detalle from "../../components/detalle";
+import { getCookies } from "cookies-next";
 
-const Lotes = () => {
-  const response = require("../api/fakeApiLotes.json");
-  const data = response.data;
+const Lotes = ({ requestOptions }) => {
+  const [data, setData] = useState([]);
+  const [fechaSeleccionada, setFechaSeleccionada] = useState(new Date());
   const [estadoModal, setEstadoModal] = useState(
     Array(data.length).fill(false)
   );
 
-  data.sort(function (a, b) {
-    var dateA = new Date(a.lotefecha),
-      dateB = new Date(b.lotefecha);
-    return dateB - dateA;
-  });
+  const getDataLotes = async (e) => {
+    setFechaSeleccionada(e.target.value);
+    let _token = getCookies();
+    console.log(_token);
+    try {
+      let url = "https://ms.nucleodediagnostico.com/trace/web/findLotes";
+      let fechaUnix = Math.floor(new Date(e.target.value).getTime() / 1000);
+      if (fechaUnix) {
+        url += `?d=${fechaUnix}`;
+      }
 
+      console.log(fechaUnix);
+      const response = await fetch(url, requestOptions);
+      const data = await response.json();
+      setData(data.data);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
   return (
     <>
-      <HeaderND title='Recepción de Muestras' />
+      <HeaderND title="Recepción de Muestras" />
       <NavHome />
       <div className={styles.bgImg}></div>
       <h1 className={styles.titulo}>Lotes</h1>
+      <div className={styles.ingresarFecha}>
+        <h3>Filtrar por fecha</h3>
+        <input
+          className={styles.inputLote}
+          type="date"
+          onChange={(e) => getDataLotes(e)}
+        />
+      </div>
       <hr />
       {data.map((producto, index) => {
         return (
           <div key={index}>
-            <div className='contenedorLista'>
+            <div className="contenedorLista">
               <button
-                className='botonloteLista'
+                className="botonloteLista"
                 onClick={() => {
                   let newEstadoModal = [...estadoModal];
                   newEstadoModal[index] = !newEstadoModal[index];
@@ -49,18 +73,16 @@ const Lotes = () => {
               }}
               producto={producto}
             >
-              <div className='contenidoModal'>
+              <div className="contenidoModal">
                 <h1 className={styles.title}>
                   Lote: {producto.lotedescripcion}
                 </h1>
                 <h5>fecha: {producto.lotefecha}</h5>
-
                 {producto.loteactivo === true ? (
                   <h2 className={styles.activo}>Activo</h2>
                 ) : (
                   <h2 className={styles.inactivo}>Inactivo</h2>
                 )}
-
                 <p>Feca de inicio: {producto.lotefechainicio}</p>
                 <p>Feca de cierre: {producto.lotefechhacierre}</p>
 
@@ -70,10 +92,16 @@ const Lotes = () => {
                     <>
                       <div key={index}>
                         <hr />
-                        <p>Clave: <b>{producto.productoclave}</b></p>
+                        <p>
+                          Clave: <b>{producto.productoclave}</b>
+                        </p>
                         <p>{producto.productoimagen}</p>
-                        <p>Cantidad: <b>{producto.productocantidad}</b></p>
-                        <p>Descripción: <b>{producto.productodescripcion}</b></p>
+                        <p>
+                          Cantidad: <b>{producto.productocantidad}</b>
+                        </p>
+                        <p>
+                          Descripción: <b>{producto.productodescripcion}</b>
+                        </p>
                       </div>
                     </>
                   );
@@ -83,13 +111,27 @@ const Lotes = () => {
           </div>
         );
       })}
-      <div className='pb-5 mb-5'>
-        <div className='pb-5 mb-5'>
-          <div className='pb-5 mb-5'></div>
+      <div className="pb-5 mb-5">
+        <div className="pb-5 mb-5">
+          <div className="pb-5 mb-5"></div>
         </div>
       </div>
     </>
   );
+};
+
+Lotes.getInitialProps = async ({ req, res }) => {
+  var requestOptions = {
+    method: "GET",
+    credentials: "include",
+    headers: {
+      Cookie: req.headers.cookie,
+    },
+  };
+
+  return {
+    requestOptions,
+  };
 };
 
 export default Lotes;
