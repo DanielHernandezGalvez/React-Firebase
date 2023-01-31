d487fdeb943202076216a327834031b32430b54d
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-01-31 09:00:02 -0600

    Update samplesLote.jsx

--- trazabilidadND/samplesLote.jsx
+++ trazabilidadND/samplesLote.jsx
@@ -1,9 +1,13 @@
+// Este código es un componente de React que muestra un formulario para actualizar un lote de productos
 import React from "react";
 import styles from "../styles/home.module.css";
-import { useState, useRef } from "react";
+import { useState } from "react";
 import Swal from "sweetalert2";
 import Image from "next/image";
 
+/* La API a la que se hace la petición se especifica en la variable api_route, 
+ que es establecida a "https://dental.nucleodediagnostico.mx" o a una ruta
+ especificada en las variables de entorno */
 const api_route =
   process.env.API_ROUTE || "https://dental.nucleodediagnostico.mx";
 
@@ -11,12 +15,16 @@ export default function SamplesLote({ data, isValid, req }) {
   const [selectedProducts, setSelectedProducts] = useState([]);
   const [productos, setProductos] = useState();
 
+  /* incrementa el valor de la cantidad de un producto en 1. 
+  Recibe un parámetro i que representa el índice del producto en la lista de productos*/
   function increment(i) {
     let e = document.getElementById("producto" + i + ".productocantidad");
     let value = parseInt(e.value) + 1;
     e.value = value;
   }
 
+  /*decrementa el valor de la cantidad de un producto en 1. Recibe un parámetro i que
+  representa el índice del producto en la lista de*/ 
   function decrement(i) {
     let e = document.getElementById("producto" + i + ".productocantidad");
     let value = parseInt(e.value) - 1;
@@ -25,45 +33,23 @@ export default function SamplesLote({ data, isValid, req }) {
     } else e.value = value;
   }
 
+  //Recarga la página actual.
   const reloading = () => {
     location.reload(true);
   };
 
-  // function handleSubmit() {
-  //   if (data.productos) {
-  //     // setProductos(data.productos)
-  //     let selected = [];
-  //     data.productos.forEach((producto) => {
-  //       selected.push({
-  //         clave: producto.productoclave,
-  //         cantidad: producto.productocantidad,
-  //       });
-  //     });
-  //   }
-
-  //   setSelectedProducts(selected);
-  //   console.log("Selected products: ", selected);
-
-  //   Swal.fire({
-  //     title: "Enviado Correctamente",
-  //     icon: "success",
-  //     confirmButtonText: "Aceptar",
-  //     timer: 2000,
-  //     confirmButtonColor: "rgb(13, 110, 253)",
-  //   });
-  //   setTimeout(reloading, 2000);
-  // }
-
-  // if (data.productos) setProductos(data.productos);
-
+  /*Manejador de eventos para el envío del formulario. Recibe un parámetro event, 
+  que representa el evento del submit del formulario */
   const handleSubmit = async (event) => {
-    event.preventDefault();
-    let row = {
+    // Se ejecuta una acción para prevenir el comportamiento por defecto de submit del formulario 
+    event.preventDefault(); 
+    //luego se crea un objeto row con los valores necesarios para la petición a la API
+    let row = {     
       lotedescripcion: data.lotedescripcion,
-      usernomina: 1845,
+      usernomina: "",
       productos: [],
     };
-
+   // se rellenan los datos de la petición req y se realiza una petición fetch a la API.
     for (let i = 0; i < data.productos.length; i++) {
       let productoI = {
         productoclave: event.target["producto" + i + ".productoclave"].value,
@@ -72,18 +58,19 @@ export default function SamplesLote({ data, isValid, req }) {
       };
       row.productos.push(productoI);
     }
-
     req.body = JSON.stringify(row);
     req.method = "POST"
     console.log(req)
     await fetch(api_route + "/trace/web/updLote", req)
       .then(async (response) => {
         console.log(await response.text().then());
+    // Si la respuesta es satisfactoria, se muestra una alerta de éxito y se recarga la página
         if (response.status === 200) {
         }
       })
+    //En caso de error, se muestra en la consola
       .catch((error) => console.log("error", error));
-
+    // Sedespliega una alerta en caso de éxito al enviar  
       Swal.fire({
             title: "Enviado Correctamente",
             icon: "success",
@@ -96,20 +83,24 @@ export default function SamplesLote({ data, isValid, req }) {
 
   return (
     <>
+    {/* Se asegura que solo se renderiza el componente si la propiedad lotedescripcion existe. */}
       {data.lotedescripcion && (
         <div className={styles.boxSamples}>
           <h2 className={styles.title4}>
-            <span className={styles.spanTitle}>Lote: </span>
-            <span id='lotedescripcion'>{data.lotedescripcion}</span>
+            <span className={styles.spanTitle}>Lote: </span>             
+            <span id='lotedescripcion'>{data.lotedescripcion}</span>     
           </h2>
           <form onSubmit={handleSubmit}>
             <div className='container mx-auto'>
               <p>Fecha: {data.lotefecha}</p>
+    {/* determina si el lote está activo o no y muestra un mensaje correspondiente. */}
               {data.loteactivo === true ? (
                 <h2 className={styles.activo}>Activo</h2>
               ) : (
                 <h2 className={styles.inactivo}>Inactivo</h2>
               )}
+    {/* determina el estatus del lote y muestra un mensaje correspondiente. 
+    Si el estatus no es 5 ni 6, se muestra un mensaje de estatus indefinido. */}
               {data.loteestatus === 5 ? (
                 <h3 className={styles.estatus + " text-primary"}>
                   En traslado a laboratorio
@@ -124,8 +115,8 @@ export default function SamplesLote({ data, isValid, req }) {
                   Estatus indefinido
                 </h3>
               )}
-
               <hr />
+    {/* Se renderiza una lista de productos asociados al lote: Clave, Descripción Cantidad e Imagen */}
               {data.productos.map((producto, index) => {
                 return (
                   <div className={styles.fila} key={index}>
@@ -145,6 +136,8 @@ export default function SamplesLote({ data, isValid, req }) {
                       </div>
                       <div>
                         <div className={styles.countContainer}>
+      {/* Se incluyen dos botones de incremento y decremento de la cantidad de cada producto,  
+      que están habilitados solo si el lote está activo y su estatus es 5.*/}
                           <button
                             onClick={() => decrement(index)}
                             className={styles.buttonRestar}
@@ -191,11 +184,12 @@ export default function SamplesLote({ data, isValid, req }) {
                 );
               })}
             </div>
+      {/* Finalmente, si el lote está activo y su estatus es 5, se muestra un botón para enviar la información */}
             {data.loteactivo === true &&
               (data.loteestatus === 5 ? (
                 <div className='container text-center'>
                   <button
-                    // onClick={() => handleSubmit()}
+                    onClick={() => handleSubmit()}
                     className='btn btn-success btn-lg mb-5 mt-5 mx-2 '
                     type='submit'
                     disabled={
@@ -206,8 +200,7 @@ export default function SamplesLote({ data, isValid, req }) {
                   </button>
                   <div className="mb-5 pb-5"></div>
                 </div>
-                
-              ) :             <div className="mb-5 pb-5"></div>
+              ) : <div className="mb-5 pb-5"></div>  
               )}
           </form>
         </div>
@@ -215,6 +208,5 @@ export default function SamplesLote({ data, isValid, req }) {
     </>
   );
 }
-
 
 
