e9ab9ffbe0097a42afeefd5d5d8dd2bee3c0c9d4
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-02-22 09:09:11 -0600

    Create buscador.js

---
+++ nucleoRH/buscador.js
@@ -0,0 +1,38 @@
+// const response = await fetch("./fakeApi.json");
+// const user = await response.json();
+
+window.addEventListener("load", async () => {
+  let requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+
+  let url = "https://ms.nucleodediagnostico.com/caphumano/checador/GetAllPeriodos"
+  const response = await fetch(url, requestOptions);
+  const data = await response.json();
+
+  console.log(data)
+
+  const option = document.getElementById("inputCorte");
+  const options = { day: 'numeric', month: 'long', year: 'numeric' };
+
+  let cortes = ""
+  for (let i = 0; i < data.length; i++) {
+    let corte = data[i];
+
+    let cerrado = "";
+    if(corte.fechacerrado.Valid) cerrado = ", cerrado el " + new Date(corte.fechacerrado).toLocaleDateString('es-MX', options);
+
+    let corteinicio = new Date(corte.fechadesde.replace("Z", ""))
+    let cortefin = new Date(corte.fechahasta.replace("Z", ""))
+    let hoy = new Date()
+    let selected = ""
+    cortefin.setUTCHours(23)
+
+    if(hoy >= corteinicio && hoy <= cortefin)  selected = "selected"
+    cortes += `
+    <option class="fechas-imput" value="${corte.id}" ${selected}>${corte.num}: del ${corteinicio.toLocaleDateString('es-MX', options)} al ${cortefin.toLocaleDateString('es-MX', options)} ${cerrado}</span></option>
+    `;
+  }
+  option.innerHTML = cortes;
+});
