f555267f81afbce41492ae2d2bc2c72523e41588
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-02-28 09:10:38 -0600

    Update checadasManual.js

--- nucleoRH/checadasManual.js
+++ nucleoRH/checadasManual.js
@@ -2,7 +2,7 @@
 // ✔ agregar botón offcanvas live demo btn normal que se abra desde el detalle del usuario que se despliegue desde la derecha
 // ✔ dentro del canvas poner un input para poner fecha, hora y confirmación (poner alerta), que tenga nombre y numero de nomina
 // ✔ ocultar botón de + cuando no haya tabla desplegada
-// para agregar checada se avisa que se debe recargar la página
+// ✔ para agregar checada se avisa que se debe recargar la página
 // ✔ en el aviso de se recargaron correctamente poner aviso que que se debe recargar
 // ✔ la fecha de incerción no puede ser mayor a hoy
 // ✔ hacer filtro por año, agregar otro input del lado izquierdo para que se filtren los periodos
