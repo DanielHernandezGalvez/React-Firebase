6347b04b60ead0a910d7295943a2f81689c8b188
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-03-14 17:48:25 -0600

    Create Contenedor.jsx

---
+++ monitorDeTomas/componentes/Contenedor.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Control from './Control'
+import Tabla from './Tabla'
+
+export default function Contenedor() {
+  return (
+    <>
+    <Control />
+    <Tabla />
+    </>
+  )
+}
