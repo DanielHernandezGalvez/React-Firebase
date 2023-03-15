9edec107f7a22c78878cf51fb8c1c66011728766
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-03-15 17:59:14 -0600

    Create fuciones.js

---
+++ monitorDeTomas/funciones/fuciones.js
@@ -0,0 +1,37 @@
+export const TABLE_COLUMNS = [
+  {
+    name: "Orden de trabajo",
+    selector: "orden",
+    className: "border",
+  },
+  {
+    name: "Nombre del Paciente",
+    selector: "nombre",
+    className: "border",
+  },
+  {
+    name: "Fecha",
+    selector: "fecha",
+    className: "border",
+  },
+  {
+    name: "Fecha Esperada",
+    selector: "fechaesperada",
+    className: "border",
+  },
+  {
+    name: "",
+    cell: (row) => (
+      <button
+        className="btn btn-primary"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target={`#row-${row.id}`}
+        aria-expanded="false"
+        aria-controls={`row-${row.id}`}
+      >
+        ver
+      </button>
+    ),
+  },
+];
