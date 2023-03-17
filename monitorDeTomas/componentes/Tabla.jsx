bb464e10ffe59d6a2369c3682af4e634a0273494
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-03-17 17:30:08 -0600

    Update Tabla.jsx

--- monitorDeTomas/componentes/Tabla.jsx
+++ monitorDeTomas/componentes/Tabla.jsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
-// import data from "../../api/api.json";
 import BuscadorHeader from "./BuscadorHeader";
 import TablaDatos from "./TablaDatos";
 import { TABLE_COLUMNS } from "./funciones/columns";
 import { filterData, filterSucursal } from "./funciones/filtrar";
 
 export default function Tabla() {
+  // EL estado filtrado inicia como array vacío que se actualiza por la funcion setFiltrado
   const [filtrado, setFiltrado] = useState([]);
 
+  /* el useEffect realiza una petición GET a la api mediante fetch y actualiza el estado
+  filtrado  con los datos de la url */
   useEffect(() => {
     const fetchData = async () => {
       let requestOptions = {
@@ -22,12 +24,16 @@ export default function Tabla() {
     fetchData();
   }, []);
 
+  /* Filtra los datos por nombre desde el componente BuscadorHeader.
+  llama a la función filterData desde la carpeta funciones */
   const handleFilter = (event) => {
     const newData = filterData(event, filtrado);
     setFiltrado(newData);
-    // console.log(newData)
   };
 
+  /* Filtra los datos por sucursales desde el componente BuscadorHeader 
+  toma un onChange de un collapse de bootstrap y usa la función filterSucursal
+  desde la carpeta funciones */
   const handleSucursal = (event) => {
     const selected = event.target.value;
     const newData = filterSucursal(selected, filtrado);
@@ -37,10 +43,11 @@ export default function Tabla() {
   return (
     <>
       <div className='container-fluid'>
-        <BuscadorHeader
+        <BuscadorHeader // el componente trae por props las funciones de filtrar.js
           handleFilter={handleFilter}
           handleSucursal={handleSucursal}
         />
+        {/* trae por props el contenido de las columnas y convierte data en el estado filtrado */}
         <TablaDatos columns={TABLE_COLUMNS} data={filtrado} />
       </div>
     </>
