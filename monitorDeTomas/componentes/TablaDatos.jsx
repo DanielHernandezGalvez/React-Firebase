a83f52d16967751d1c07b739331976ccef866fa9
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-03-23 09:17:22 -0600

    Update TablaDatos.jsx

--- monitorDeTomas/componentes/TablaDatos.jsx
+++ monitorDeTomas/componentes/TablaDatos.jsx
@@ -1,75 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import DataTable from "react-data-table-component";
-import { Accordion, Card } from "react-bootstrap";
-
-const DetalleFila = ({ data }) => {
-  
-  return (
-    <Accordion className='border' defaultActiveKey='0'>
-      <Card>
-        <Accordion.Toggle as={Card.Header} eventKey='0'>
-          Detalles
-        </Accordion.Toggle>
-        <Accordion.Collapse eventKey='0'>
-          <Card.Body>
-            <tr>
-              <td>Estudio</td>
-              <td>{data.Estudio}</td>
-            </tr>
-            <tr>
-              <td>Muestra</td>
-              <td>{data.Muestra}</td>
-            </tr>
-            <tr>
-              <td>Contenedor</td>
-              <td>{data.Contenedor}</td>
-            </tr>
-            <tr>
-              <td>Etiquetas</td>
-              <td>{data.CantidadEtiquetas}</td>
-            </tr>
-            <tr>
-              <td>Tiempo Estimado</td>
-              <td>{data.TiempoEstimado}</td>
-            </tr>
-            <tr>
-              <td>Fecha Estimada</td>
-              <td>{data.FechaEstimada}</td>
-            </tr>
-            <tr>
-              <td>Urgencia</td>
-              <td>{data.Urgente}</td>
-            </tr>
-            <tr>
-              <td>Área</td>
-              <td>{data.Área}</td>
-            </tr>
-            <tr>
-              <td>Observaciones</td>
-              <td>{data.Observaciones.String}</td>
-            </tr>
-          </Card.Body>
-        </Accordion.Collapse>
-      </Card>
-    </Accordion>
-  );
-};
+import DataTableExtensions from "react-data-table-component-extensions";
+import "react-data-table-component-extensions/dist/index.css";
+import DetalleFila from "./DetalleFila";
 
 const TablaDatos = ({ columns, data }) => {
+  const [expandedRows, setExpandedRows] = useState({});
+
+  // Función para manejar el cambio de estado de los expandibles
+  const handleRowExpand = (row, expanded) => {
+    const expandedRowsCopy = { ...expandedRows };
+    expandedRowsCopy[row.index] = expanded;
+    setExpandedRows(expandedRowsCopy);
+  };
   
+  const tableData = {
+    columns: columns,
+    data: data,
+    expandableRows: true,
+    expandableRowsComponent: DetalleFila,
+    expandOnRowClicked: true,
+    onRowExpandToggled: handleRowExpand,
+    expandedRows: expandedRows
+  };
+
   return (
     <div className='container-fluid'>
-      <DataTable
-        columns={columns}
-        data={data}
-        pagination
-        highlightOnHover
-        responsive
-        fixedHeader
-        expandableRows
-        className='border-top border-start'
-        expandableRowsComponent={<DetalleFila />}
-      ></DataTable>
+      <DataTableExtensions {...tableData}>
+        <DataTable
+          columns={columns}
+          data={data}
+          pagination
+          highlightOnHover
+          striped
+          responsive
+          fixedHeader
+          expandableRows
+          className='border-top border-start'
+          expandableRowsComponent={DetalleFila}
+        ></DataTable>
+      </DataTableExtensions>
     </div>
   );
 };
