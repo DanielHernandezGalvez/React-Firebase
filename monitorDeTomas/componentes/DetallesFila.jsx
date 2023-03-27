29a968f4d61db1ba9263a9a74870746a197c5a54
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-03-27 09:15:58 -0600

    Update DetallesFila.jsx

--- monitorDeTomas/componentes/DetallesFila.jsx
+++ monitorDeTomas/componentes/DetallesFila.jsx
@@ -1,69 +1,107 @@
-import { Accordion, Card } from "react-bootstrap";
 import { useEffect, useState } from "react";
 
-
-const  DetalleFila = ({ data }) => {
-  const [detale, setDetalle] = useState([]);
+const DetalleFila = ({ data }) => {
+  const [detalle, setDetalle] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8081/sian2/ms/monitor/GetAllOrdenesTrabajo?Id=8421625")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setDetalle(data.data);
-      })
-      .catch((error) => console.log(error));
+    const fetchOrdenesTrabajo = async () => {
+      let requestOptions = {
+        method: "GET",
+        redirect: "follow",
+      };
+      try {
+        // console.log(data.OrdenTrabajoId);
+        const response = await fetch(
+          `http://localhost:8081/sian2/ms/monitor/GetAllOrdenesTrabajo?Id=${data.OrdenTrabajoId}`,
+          requestOptions
+        );
+        const data_d = await response.json();
+        console.log(data_d);
+        setDetalle(data_d.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchOrdenesTrabajo();
   }, []);
 
   return (
-    <Accordion className='border' defaultActiveKey='0'>
-      <Card>
-        <Accordion.Toggle as={Card.Header} eventKey='0'>
-          Detalles
-        </Accordion.Toggle>
-        <Accordion.Collapse eventKey='0'>
-          <Card.Body>
-            <tr>
-              <td>Estudio</td>
-              <td>{data.Estudio}</td>
-            </tr>
-            <tr>
-              <td>Muestra</td>
-              <td>{data.Muestra}</td>
-            </tr>
-            <tr>
-              <td>Contenedor</td>
-              <td>{data.Contenedor}</td>
-            </tr>
-            <tr>
-              <td>Etiquetas</td>
-              <td>{data.CantidadEtiquetas}</td>
-            </tr>
-            <tr>
-              <td>Tiempo Estimado</td>
-              <td>{data.TiempoEstimado}</td>
-            </tr>
-            <tr>
-              <td>Fecha Estimada</td>
-              <td>{data.FechaEstimada}</td>
-            </tr>
-            <tr>
-              <td>Urgencia</td>
-              <td>{data.Urgente}</td>
-            </tr>
-            <tr>
-              <td>Área</td>
-              <td>{data.Área}</td>
-            </tr>
-            <tr>
-              <td>Observaciones</td>
-              <td>{data.Observaciones.String}</td>
-            </tr>
-          </Card.Body>
-        </Accordion.Collapse>
-      </Card>
-    </Accordion>
+    <table className='table table-sm border'>
+      {console.log(data)}
+      <thead>
+        <tr className='bg-secondary text-white text-center'>
+          <th scope='row' className='border-end  '>
+            Estudio
+          </th>
+          <th className='border-end '>Muestra</th>
+          <th className='border-end '>Contenedor</th>
+          <th className='border-end '>Etiquetas</th>
+          <th className='border-end '>T. Estimado</th>
+          <th className='border-end '>F. Estimado</th>
+          <th className='border-end '>Urgente</th>
+          <th className='border-end '>Area</th>
+          <th className=''>Observaciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        {detalle.map((det) => {
+          return (
+            <>
+              <tr className='text-center'>
+                <th scope='row' className=' border-end fw-normal'>
+                  {det.Estudio}
+                </th>
+                <td className=' border-end'>{det.Muestra}</td>
+                <td className=' border-end'>{det.Contenedor}</td>
+                <td className=' border-end'>{det.CantidadEtiquetas}</td>
+                <td className=' border-end'>{det.TiempoEstimado}</td>
+                <td className=' border-end'>{det.FechaEstimada}</td>
+                <td className=' border-end'>
+                  {/* <input type='checkbox' /> */}
+                  {det.Urgente}
+                </td>
+                <td className=' border-end'>{det.Área}</td>
+                <td className='border-end '>PTE</td>
+              </tr>
+
+              <td colspan='9' className='accordion ' id='accordionExample'>
+                <div className='accordion-item'>
+                  <h2 className='accordion-header text-center' id='headingOne'>
+                    <button
+                      className='accordion-button btn btn-secondary py-2 collapsed px-5 fw-bold'
+                      type='button'
+                      data-bs-toggle='collapse'
+                      data-bs-target='#collapseOne'
+                      aria-expanded='false'
+                      aria-controls='collapseOne'
+                    >
+                      PREPARACIÓN
+                    </button>
+                  </h2>
+                  <div
+                    id='collapseOne'
+                    className='accordion-collapse collapse '
+                    aria-labelledby='headingOne'
+                    data-bs-parent='#accordionExample'
+                  >
+                    <div className='accordion-body text-center'>
+                      <p className='px-5'>
+                        ORINA RECIENTE, NO REQUIERE AYUNO, MATRIZ LUNES A
+                        VIERNES 7 DE A MAÑANA A 7 DE LA TARDE Y SABADO DE 7 DE LA
+                        MAÑANA A 2 DE LA TARDE Y SUCURSALES DE 7 DE LA MAÑANA A
+                        6 DE LA TARDE Y SABADO DE 7 DE LA MAÑANA A 1 DE LA TARDE
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </td>
+            </>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
 
-export default DetalleFila
+export default DetalleFila;
