2ea6fef6f5b39d8431271826aed3c51c7ee0d960
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-03-28 09:04:45 -0600

    Update columns.js

--- monitorDeTomas/funciones/columns.js
+++ monitorDeTomas/funciones/columns.js
@@ -1,38 +1,110 @@
+/*  Cada objeto representa una columna de una tabla y tiene las siguientes propiedades:
+"name": el nombre de la columna
+"selector": el nombre de la propiedad del objeto que se usará para llenar la columna
+"className": una clase CSS que se aplicará a la celda de la columna en Bootstrap
+"sortable": un valor booleano que indica si la columna es ordenable o no
+"cell": una función que se usa para personalizar el contenido de la celda de la columna */
 export const TABLE_COLUMNS = [
   {
     name: "Orden de trabajo",
-    selector: "orden",
+    selector: "OrdenDeTrabajo",
     className: "border",
+    sortable: true,
   },
   {
     name: "Nombre del Paciente",
-    selector: "nombre",
+    selector: "NombrePaciente",
     className: "border",
+    sortable: true,
   },
   {
-    // Formateo de fechas
     name: "Fecha",
-    selector: "fecha",
+    selector: "Fecha",
     className: "border",
+    sortable: true,
+    /* La función "cell" se usa en las dos últimas columnas para formatear las 
+    fechas a la forma deseada, en caso de que el número sea menor a 10, se le 
+    agrega un cero anes del número */
+    cell: (row) => {
+      const fecha = new Date(row.Fecha);
+      const dia = fecha.getDate().toString().padStart(2, "0");
+      const mes = (fecha.getMonth() + 1).toString().padStart(2, "0");
+      const anio = fecha.getFullYear();
+      const fechaConFormato = `${dia}/${mes}/${anio}`;
+      return <div>{fechaConFormato}</div>;
+    },
   },
   {
     name: "Fecha Esperada",
-    selector: "fechaesperada",
-    className: "border",
+    selector: "FechaEstimada.Time",
+    className: "",
+    sortable: true,
+    /* La función "cell" se usa en las dos últimas columnas para formatear las 
+    fechas a la forma deseada, en caso de que el número sea menor a 10, se le 
+    agrega un cero anes del número */
+    cell: (row) => {
+      const fechaEstimada = new Date(row.FechaEstimada.Time);
+      const anio = fechaEstimada.getUTCFullYear();
+      const dia = fechaEstimada.getDate().toString().padStart(2, "0");
+      const mes = (fechaEstimada.getMonth() + 1).toString().padStart(2, "0");
+      const fechaFormateada = `${dia}/${mes}/${anio}`;
+      return <div>{fechaFormateada}</div>;
+    },
+  },
+];
+
+export const TABLA_PRINCIPAL_COLUMNS = [
+  {
+    name: "Producto",
+    selector: "ProductoImagen",
+    sortable: true,
+    cell: (row) => (
+      <img
+        // src={row.ProductoImagenUrl}
+        src={
+          "https://dhb3yazwboecu.cloudfront.net/764//laboratorio/frasco-orina-125-ml.jpg"
+        }
+        alt={row.ProductoDescripcion}
+        className='tabla-img'
+      />
+    ),
   },
   {
-    name: "",
+    name: "Descripcion",
+    selector: "ProductoDescripcion",
+    sortable: true,
+  },
+  {
+    name: "Estudios",
+    selector: "Estudios",
+    sortable: true,
+  },
+  {
+    name: "Fecha",
+    selector: "Fecha",
+    sortable: true,
+    cell: (row) => {
+      const fechaOriginal = "2023-03-10";
+      const partesFecha = fechaOriginal.split("-");
+      const dia = partesFecha[2];
+      const mes = partesFecha[1];
+      const año = partesFecha[0];
+      const fechaConvertida = `${dia}/${mes}/${año}`;
+      return <p>{fechaConvertida}</p>;
+    },
+  },
+  {
+    name: "Imprimir",
     cell: (row) => (
       <button
-        className="btn btn-primary"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target={`#row-${row.id}`}
-        aria-expanded="false"
-        aria-controls={`row-${row.id}`}
+        className='btn btn-secondary'
+        onClick={() => console.log(`Imprimir ${row.ProductoDescripcion}`)}
       >
-        ver
+        Imprimir
       </button>
     ),
+    ignoreRowClick: true,
+    allowOverflow: true,
+    button: true,
   },
 ];
