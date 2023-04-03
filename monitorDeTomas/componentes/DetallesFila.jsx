1029072ee15d69dceccba327b51f324f9c1ef9c2
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-04-03 11:16:51 -0600

    Update DetallesFila.jsx

--- monitorDeTomas/componentes/DetallesFila.jsx
+++ monitorDeTomas/componentes/DetallesFila.jsx
@@ -1,121 +1,10 @@
-// import { useEffect, useState } from "react";
-
-// //============== 3FE0321141 ===============//
-
-// const DetalleFila = ({ data }) => {
-//   const [detalle, setDetalle] = useState([]);
-
-//   // useEffect( () => {
-//   //   console.log(data.OrdenTrabajoId);
-//   //   fetch(
-//   //     "http://localhost:8081/sian2/ms/monitor/GetAllOrdenesTrabajo?Id=" + data.OrdenTrabajoId
-//   //   )
-//   //     .then((response) => response.json())
-//   //     .then((data_d) => {
-//   //       console.log(data_d);
-//   //       setDetalle(data.data);
-//   //     })
-//   //     .catch((error) => console.log(error));
-//   // }, []);
-
-//   useEffect(() => {
-//     const fetchOrdenesTrabajo = async () => {
-//       let requestOptions = {
-//         method: "GET",
-//         redirect: "follow",
-//       };
-//       try {
-//         // console.log(data.OrdenTrabajoId);
-//         const response = await fetch(
-//           `http://localhost:8081/sian2/ms/monitor/GetAllOrdenesTrabajo?Id=${data.OrdenTrabajoId}`,
-//           requestOptions
-//         );
-//         const data_d = await response.json();
-//         console.log(data_d);
-//         setDetalle(data_d.data);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     };
-
-//     fetchOrdenesTrabajo();
-//   }, []);
-
-//   return (
-//     <table className='table table-sm border'>
-//       {console.log(data)}
-//       <thead>
-//         <tr className='bg-secondary text-white text-center'>
-//           <th scope='row' className='border-end  '>
-//             Estudio
-//           </th>
-//           <th className='border-end '>Muestra</th>
-//           <th className='border-end '>Contenedor</th>
-//           <th className='border-end '>Etiquetas</th>
-//           <th className='border-end '>T. Estimado</th>
-//           <th className='border-end '>F. Estimada</th>
-//           <th className='border-end '>Urgente</th>
-//           <th className='border-end '>Área</th>
-//           <th className=''>Observaciones</th>
-//         </tr>
-//       </thead>
-//       <tbody className="text-sm-start">
-//         {detalle.map((det) => {
-
-//           return (
-//             <>
-//               <tr className='text-center'>
-//                 <th scope='row' className='d-flex ps-3 border-end fw-normal'>
-//                   {/* COLLAPSE */}
-//                   <div>
-//                     <button
-//                       className='btn btn-white me-3 px-2s py-1 border-none'
-//                       type='button'
-//                       data-bs-toggle='collapse'
-//                       data-bs-target='#collapseExample'
-//                       aria-expanded='false'
-//                       aria-controls='collapseExample'
-//                     >
-//                       +
-//                     </button>
-//                   </div>
-//                   <div scope="row">
-//                   <div className='collapse' id='collapseExample'>
-//                     <div className='card card-body'>
-//                      {det.Observaciones.String}
-//                     </div>
-//                   </div>{" "}
-//                   </div>
-//                   {/* fin del colapse */}
-//                   {det.Estudio}
-//                 </th>
-//                 <td className=' border-end'>{det.Muestra}</td>
-//                 <td className=' border-end'>{det.Contenedor}</td>
-//                 <td className=' border-end'>{det.CantidadEtiquetas}</td>
-//                 <td className=' border-end'>{det.TiempoEstimado != 1 ? <p>{det.TiempoEstimado} Días</p> : <p>{det.TiempoEstimado} Día</p>}</td>
-//                 <td className=' border-end'>{new Date(det.FechaEstimada).toLocaleDateString('es-ES', {day: '2-digit', month: '2-digit', year: 'numeric'}).replace(/\//g, "/")}</td>
-//                 <td className=' border-end'>
-//                   {det.Urgente ? <p>Urgente</p> : <p>No Urgente</p>}
-//                 </td>
-//                 <td className='border-end'>{det.Área}</td>
-//                 <td className='border-end '>{det.Estatus}</td>
-//               </tr>
-//             </>
-//           );
-//         })}
-//       </tbody>
-//     </table>
-//   );
-// };
-
-// export default DetalleFila;
-
 import React, { useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
 import { COLUNS_DETAILS_TABLE } from "./funciones/columns";
 
 const DetalleFila = ({ data }) => {
   const [detalle, setDetalle] = useState([]);
+  const [filaExpandibe, setFilaExpandible] = useState(null);
 
   useEffect(() => {
     const fetchOrdenesTrabajo = async () => {
@@ -149,10 +38,19 @@ const DetalleFila = ({ data }) => {
       sortable: true,
     },
   ];
-  const expandableRowsComponent = () => {
+  const expandableRowsComponent = ({ data, rowIndex }) => {
+    // const preparacionData = detalle.filter(
+    //   (d) => d.ProductoId === data.ProductoId
+    // );
+
+    if (rowIndex === filaExpandibe) {
+      return null;
+    }
+
     const preparacionData = detalle.filter(
       (d) => d.ProductoId === data.ProductoId
     );
+
     return (
       <div>
         <DataTable
@@ -168,6 +66,12 @@ const DetalleFila = ({ data }) => {
     );
   };
 
+  /* compara si existe igualdad con la función anterior para descartar si ya 
+  hay un expandible abierto, toma el rowIndex como parametro principal */
+  const handleRowExpand = (rowIndex) => {
+    setFilaExpandible(filaExpandibe === rowIndex ? null : rowIndex);
+  };
+
   return (
     <>
       <DataTable
@@ -175,6 +79,10 @@ const DetalleFila = ({ data }) => {
         data={detalle}
         expandableRows
         expandableRowsComponent={expandableRowsComponent}
+        expandableRowExpanded={(row) =>
+          row.ProductoId === detalle[filaExpandibe]?.ProductoId
+        }
+        onRowExpandToggled={handleRowExpand}
         highlightOnHover
         striped
         responsive
