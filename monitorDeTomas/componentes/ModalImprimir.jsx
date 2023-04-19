ef9253002c28e22d0b6bd9671712f3ab5286ae27
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-04-19 09:04:09 -0600

    Update ModalImprimir.jsx

--- monitorDeTomas/componentes/ModalImprimir.jsx
+++ monitorDeTomas/componentes/ModalImprimir.jsx
@@ -2,63 +2,54 @@ import React, { useState, useEffect } from "react";
 import DataTable from "react-data-table-component";
 import ReactDom from "react-dom";
 
-const ModalImprimir = ({show, onClose, recipientes}) => {
-  const showHideClassName = show
-    ? "modal display-block"
-    : "modal display-none";
-  // const [data, setData] = useState(null);
-  // const [params, setParams] = useState({ f: "", Ip: "", P: "" });
-
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     try {
-  //       const url = `http://192.168.0.14:8081/sian2/ms/monitor/MandarPdfAImprimir?f=${params.f}&Ip=${params.Ip}&P=${params.P}`;
-  //       const requestOptions = {
-  //         method: "GET",
-  //         redirect: "follow",
-  //       };
-  //       const response = await fetch(url, requestOptions);
-  //       const result = await response.text();
-  //       setData(result);
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   }
-  //   fetchData();
-  // }, [params]);
-
-  // const handleDataClick = (data) => {
-  //   // checar los valores de IpValue y Etiquetas
-  //   setParams({ f: data.Folio, Ip: data.IpValue, P: data.Etiquetas })
-  // }
+const ModalImprimir = ({ show, onClose, recipientes }) => {
+  
+  const handleInputChange = (event) => {
+    const inputValue = parseInt(event.target.value);
+    if (inputValue >= 1) {
+      setValue(inputValue);
+    }
+  };
 
   return (
-    <div className={showHideClassName}>
+    <div
+      className='modal fade'
+      id='modalprint'
+      tabindex='-1'
+      aria-labelledby='exampleModalLabel'
+      aria-hidden='true'
+    >
       <div className='modal-dialog modal-dialog-centered'>
         <div className='modal-content'>
           <div className='modal-header'>
-            <h5 className='modal-title'>
-              {recipientes.ProductoDescripcion}
+            <h5 className='modal-title' id='printProductoDescripcion'>
+              recipientes.ProductoDescripcion
             </h5>
-            <button
+            {/* <button
               type='button'
               className='btn-close'
               onClick={onClose}
-            ></button>
+            ></button> */}
           </div>
           <div className='modal-body'>
-            <p>{recipientes.Estudio}</p>
-            <p>{recipientes.Fecha}</p>
+            <p id='printFolio'>Folio</p>
+            <p id='printEstudios'>recipientes.Estudios</p>
+            <input
+              id='inputToprint'
+              className='form-control'
+              type='number'
+              min='1'
+            />
           </div>
           <div className='modal-footer'>
             <button
               type='button'
               className='btn btn-secondary'
-              onClick={onClose}
+              data-bs-dismiss='modal'
             >
               Cerrar
             </button>
-            <button type='button' className='btn btn-primary'>
+            <button type='button' className='btn btn-primary' onClick={()=>{print(document.getElementById('inputToprint').ariaLabel, parseInt(document.getElementById('inputToprint').value))}}>
               Imprimir
             </button>
           </div>
@@ -66,6 +57,15 @@ const ModalImprimir = ({show, onClose, recipientes}) => {
       </div>
     </div>
   );
+};
+
+const print = (url, cantidad) => {
+  console.log(cantidad)
+
+  fetch(url)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
 };
 
 export default ModalImprimir;
