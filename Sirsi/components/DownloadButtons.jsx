7c0087d7e27c104edb7f0def336f8129d0b8e5dd
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-04-26 12:27:59 -0600

    Update DownloadButtons.jsx

--- Sirsi/components/DownloadButtons.jsx
+++ Sirsi/components/DownloadButtons.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export default function DowloadButtons(props) {
-  
   return (
     <div className='col-2 h-50 '>
-    <div className="btn-group ms-5 ps-5">
-    <button onClick={props.handleExportToPDF} className="btn btn-danger py-1 px-2"><i class="bi bi-filetype-pdf text-white fs-4"></i></button>
-    <button onClick={props.handleExportToExcel} className="btn btn-success p-1 px-2"><i class="bi bi-file-earmark-spreadsheet text-white fs-4"></i></button>
-    <button onClick={props.handlePrintTable} className="btn btn-secondary p-1 px-2"><i class="bi bi-printer text-white fs-4"></i></button>
+      <div className='btn-group ms-5 ps-5'>
+        <button
+          onClick={props.handleExportToPDF}
+          className='btn btn-danger py-1 px-2'
+        >
+          <i className='bi bi-filetype-pdf text-white fs-4'></i>
+        </button>
+        <button
+          onClick={props.handleExportToExcel}
+          className='btn btn-success p-1 px-2'
+        >
+          <i className='bi bi-file-earmark-spreadsheet text-white fs-4'></i>
+        </button>
+        <button
+          onClick={props.handlePrintTable}
+          className='btn btn-secondary p-1 px-2'
+        >
+          <i className='bi bi-printer text-white fs-4'></i>
+        </button>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
