28781993a739d2fa5c81a6cd481186f426c0b43b
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-04-29 09:04:53 -0600

    Update DownloadButtons.jsx

--- Sirsi/components/DownloadButtons.jsx
+++ Sirsi/components/DownloadButtons.jsx
@@ -2,22 +2,22 @@ import React from "react";
 
 export default function DowloadButtons(props) {
   return (
-    <div className='col-2 h-50 '>
+    <div className=' h-50 '>
       <div className='btn-group ms-5 ps-5'>
         <button
-          onClick={props.handleExportToPDF}
+          onClick={handleExportToPDF}
           className='btn btn-danger py-1 px-2'
         >
           <i className='bi bi-filetype-pdf text-white fs-4'></i>
         </button>
         <button
-          onClick={props.handleExportToExcel}
+          onClick={handleExportToExcel}
           className='btn btn-success p-1 px-2'
         >
           <i className='bi bi-file-earmark-spreadsheet text-white fs-4'></i>
         </button>
         <button
-          onClick={props.handlePrintTable}
+          onClick={handlePrintTable}
           className='btn btn-secondary p-1 px-2'
         >
           <i className='bi bi-printer text-white fs-4'></i>
