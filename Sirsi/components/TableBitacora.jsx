26411bafc13107dc97a5438ee94e9eef600a7869
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-05-10 08:56:55 -0600

    Update TableBitacora.jsx

--- Sirsi/components/TableBitacora.jsx
+++ Sirsi/components/TableBitacora.jsx
@@ -173,48 +173,7 @@ export default function TableBitacora() {
     },
   ];
 
-  //   const data = [
-  //     {
-  //       id: 1,
-  //       title: "Beetlejuice",
-  //       year: "1988",
-  //     },
-  //     {
-  //       id: 2,
-  //       title: "Ghostbusters",
-  //       year: "1984",
-  //     },
-  //     {
-  //       id: 1,
-  //       title: "Beetlejuice",
-  //       year: "1988",
-  //     },
-  //     {
-  //       id: 2,
-  //       title: "Ghostbusters",
-  //       year: "1984",
-  //     },
-  //     {
-  //       id: 1,
-  //       title: "Beetlejuice",
-  //       year: "1988",
-  //     },
-  //     {
-  //       id: 2,
-  //       title: "Ghostbusters",
-  //       year: "1984",
-  //     },
-  //     {
-  //       id: 1,
-  //       title: "Beetlejuice",
-  //       year: "1988",
-  //     },
-  //     {
-  //       id: 2,
-  //       title: "Ghostbusters",
-  //       year: "1984",
-  //     },
-  //   ];
+
 
   const tableData = {
     columns: columns,
