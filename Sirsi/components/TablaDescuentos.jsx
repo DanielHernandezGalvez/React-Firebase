06166a213ae3d954e88a91e960e7baf134627840
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-05-16 09:04:07 -0600

    Create TablaDescuentos.jsx

---
+++ Sirsi/components/TablaDescuentos.jsx
@@ -0,0 +1,120 @@
+import React, {useEffect, useState} from 'react'
+import FiltrosDescuentos from './FiltroDescuentos'
+import DataTable from 'react-data-table-component';
+import DataTableExtensions from "react-data-table-component-extensions";
+import "react-data-table-component-extensions/dist/index.css";
+import ResumenDescuentos from './ResumenDescuentos';
+
+function TablaDescuentos() {
+const [dataDescuentos, setDataDescuentos] = useState([])
+
+const capitalize = (val) => {
+    return val
+      .toLowerCase()
+      .trim()
+      .split(" ")
+      .map((v) => v[0].toUpperCase() + v.substr(1))
+      .join(" ");
+  };
+
+  const getSucursales = async () => {
+    const url = process.env.RUTA_API + "/sirsi/web/BuscarSucursales?suc=1";
+
+    try {
+      const response = await fetch(url);
+      let data = [];
+      data = await response.json();
+
+      const select = document.getElementById("sucInputDescuentos");
+      select.innerHTML = "";
+      const option = document.createElement("option");
+      option.value = 0;
+      option.selected = true;
+      option.text = "Todas";
+      select.appendChild(option);
+
+      data.data.map((sucursal) => {
+        const option = document.createElement("option");
+        option.value = sucursal.SucuId;
+        option.text = capitalize(sucursal.SucuNombre);
+        select.appendChild(option);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fechaActualDescuentos = () => {
+    const myDateInput = document.getElementById("fiInputDescuentos");
+    const ffInputBitacora = document.getElementById("ffInputDescuentos");
+    const fechaActual = new Date(new Date() - 6 * 60 * 60 * 1000).toISOString().split("T")[0];
+    myDateInput.value = fechaActual;
+    ffInputBitacora.value = fechaActual;
+  };
+
+  const getDescuentos = async (event) => {
+    event.preventDefault()
+    
+    const fiDate = new Date(document.getElementById("fiInputDescuentos").value);
+    const ffDate = new Date(document.getElementById("ffInputDescuentos").value);
+    const fi = Math.floor(fiDate.getTime() / 1000);
+    const ff = Math.floor(ffDate.getTime() / 1000);
+    const suc = document.getElementById("sucInputDescuentos").value;
+    const url = `${process.env.RUTA_API}/ruta-del-backcend`
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json()
+        console.log("click descuentos", data.data)
+        setDataDescuentos(data.data)
+      } 
+      catch (error) {
+        alert(error, "no se pudieron traer los datos")
+      }
+  };
+
+  useEffect(() => {
+    getSucursales();
+    fechaActualDescuentos();
+  }, []);
+
+  const columns = [
+    {
+        name: "",
+        selector: "",
+        sorteable: true
+    }
+  ]
+
+  const tableData = {
+    columns: columns,
+    data: dataDescuentos,
+    fileName: "document",
+    export: true,
+    print: true,
+    filterHidden: true,
+    filterDigit: 1,
+  };
+
+  return (
+    <>
+        <FiltrosDescuentos getDescuentos={getDescuentos} />   
+        <div className='col-9' > 
+        <DataTableExtensions {...tableData}>
+            <DataTable
+            columns={columns}
+            data={dataDescuentos}
+            responsive='true'
+            pagination
+            fixedHeader
+            />
+        </DataTableExtensions>
+        </div>
+        <div className='col-3'> 
+        <ResumenDescuentos />
+        </div>
+  </>
+  )
+}
+
+export default TablaDescuentos
