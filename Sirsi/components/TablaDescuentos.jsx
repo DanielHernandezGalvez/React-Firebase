539f9d4f9beb1fa638773ee786eb1ee89af8c91f
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-05-18 09:20:59 -0600

    Update TablaDescuentos.jsx

--- Sirsi/components/TablaDescuentos.jsx
+++ Sirsi/components/TablaDescuentos.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import FiltrosDescuentos from './FiltroDescuentos'
 import DataTable from 'react-data-table-component';
 import DataTableExtensions from "react-data-table-component-extensions";
@@ -6,13 +6,22 @@ import "react-data-table-component-extensions/dist/index.css";
 import ResumenDescuentos from './ResumenDescuentos';
 import GraficaDescuentos from "./GraficaDescuentos"
 import DescuentoDetalle from './DescuentoDetalle';
+import ResumenDescuentosDetalle from "./ResumenDescuentosDetalle"
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import { Card, Grid, useMediaQuery } from '@mui/material';
+import { gridSpacing } from 'store/constant';
 
-function TablaDescuentos() {
-const [dataDescuentos, setDataDescuentos] = useState([])
-const [tipoDescuento, setTipoDescuento] = useState("")
-const [impTotal, setImpTotal] = useState([])
+function TablaDescuentos({ iconPrimary, primary, secondary, secondarySub, color, bgcolor }) {
+  const [dataDescuentos, setDataDescuentos] = useState([])
+  const [tipoDescuento, setTipoDescuento] = useState("")
+  const [isOpen, setIsOpen] = useState(false)
+  const [dataDetalle, setDataDetalle] = useState([])
 
-const capitalize = (val) => {
+  const theme = useTheme();
+  const matchDownXs = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const capitalize = (val) => {
     return val
       .toLowerCase()
       .trim()
@@ -22,6 +31,7 @@ const capitalize = (val) => {
   };
 
   const getSucursales = async () => {
+
     const url = process.env.RUTA_API + "/sirsi/web/BuscarSucursales?suc=1";
 
     try {
@@ -58,7 +68,7 @@ const capitalize = (val) => {
 
   const getDescuentos = async (event) => {
     event.preventDefault()
-    
+
     const fiDate = new Date(document.getElementById("fiInputDescuentos").value);
     const ffDate = new Date(document.getElementById("ffInputDescuentos").value);
     const fi = Math.floor(fiDate.getTime() / 1000);
@@ -67,15 +77,18 @@ const capitalize = (val) => {
     const url = `${process.env.RUTA_API}/sirsi/web/BuscarDescuentosTotales?IdSucursal=${suc}&Fi=${fi}&Ff=${ff}`
 
     try {
-        const response = await fetch(url);
+      const response = await fetch(url);
+      if (response.status === 200) {
         const data = await response.json()
-        console.log("click descuentos", data.data)
+        // console.log("click descuentos", data.data)
         setDataDescuentos(data.data)
-        setImpTotal(data.data.ImporteTotal)
-      } 
-      catch (error) {
-        alert(error, "no se pudieron traer los datos")
+        setIsOpen(false)
+        sessionStorage.setItem('data', JSON.stringify(data.data));
       }
+    }
+    catch (error) {
+      alert(error, "no se pudieron traer los datos")
+    }
   };
 
   useEffect(() => {
@@ -83,14 +96,18 @@ const capitalize = (val) => {
     fechaActualDescuentos();
   }, []);
 
-  const activarTabla =  (td) => {
+
+  const activarTabla = (td) => {
+    // if(td === tipoDescuento) setIsOpen(false)
     setTipoDescuento(td)
+    setIsOpen(true)
+    console.log(tipoDescuento)
   }
 
   const columns = [
     {
       name: "Detalle",
-      selector: "TipoDescuento",
+      selector: (row) => row.TipoDescuento,
       sortable: true,
       cell: (row) => (
         <button className='btn' onClick={() => activarTabla(row.TipoDescuento)}>
@@ -99,29 +116,29 @@ const capitalize = (val) => {
       ),
     },
     {
-        name: "Tipo de Descuento",
-        selector: "TipoDescuento",
-        sorteable: true
+      name: "Tipo de Descuento",
+      selector: (row) => row.TipoDescuento,
+      sorteable: true
     },
     {
-        name: "N. Descuentos",
-        selector: "NumDescuentos",
-        sorteable: true
+      name: "N. Descuentos",
+      selector: (row) => row.NumDescuentos,
+      sorteable: true
     },
     {
-        name: "Importe Total",
-        selector: "ImporteTotal",
-        sorteable: true
+      name: "Importe Total",
+      selector: (row) => row.ImporteTotal,
+      sorteable: true
     },
     {
-        name: "Descuento",
-        selector: "Descuento",
-        sorteable: true
+      name: "Descuento",
+      selector: (row) => row.Descuento,
+      sorteable: true
     },
     {
-        name: "Subtotal",
-        selector: "Subtotal",
-        sorteable: true
+      name: "Subtotal",
+      selector: (row) => row.Subtotal,
+      sorteable: true
     },
   ]
 
@@ -137,32 +154,58 @@ const capitalize = (val) => {
 
   return (
     <>
-      
-        <FiltrosDescuentos getDescuentos={getDescuentos} />   
-        <div className='row' > 
-        <div className='col-12 col-lg-9' > 
-          <DataTableExtensions {...tableData}>
-            <DataTable
-            columns={columns}
-            data={dataDescuentos}
-            responsive='true'
-            pagination
-            fixedHeader
-            />
-          </DataTableExtensions>
-        </div>
-        <div className='col-12 col-lg-3 resumen-descuentos'> 
-          <ResumenDescuentos />
-          </div>
-        </div>
-        <div className='col-12'> 
-          <GraficaDescuentos />
-        </div>
-        {tipoDescuento && 
-        <DescuentoDetalle  tipoDescuento={tipoDescuento} />
-      }
-        
-  </>
+      <Grid item xs={12}>
+        <Card sx={{ bgcolor: bgcolor || '', position: 'relative' }}>
+          <FiltrosDescuentos getDescuentos={getDescuentos} />
+        </Card>
+      </Grid>
+      <Grid item xs={12}>
+        <Grid container spacing={gridSpacing}>
+          <Grid item xs={12} lg={8} md={7}>
+            <Card sx={{ bgcolor: bgcolor || '', position: 'relative' }}>
+              <DataTableExtensions {...tableData}>
+                <DataTable
+                  columns={columns}
+                  data={dataDescuentos}
+                  responsive='true'
+                  pagination
+                  fixedHeader
+                />
+              </DataTableExtensions>
+            </Card>
+          </Grid>
+          
+          <Grid item xs={12} lg={4} md={5}>
+            <Card  sx={{ bgcolor: bgcolor || '', position: 'relative' }}>
+              <h5 className='my-3 fw-bold mx-2'>Resumen</h5>
+              </Card>
+          </Grid>
+              <Grid item xs={12} lg={4} md={5}>
+            <Card  sx={{ bgcolor: bgcolor || '', position: 'relative' }}>
+              {dataDescuentos.length > 0 ? <ResumenDescuentos dataDescuentos={dataDescuentos} /> : []}
+              </Card>
+          </Grid>
+              <Grid item xs={12} lg={4} md={5}>
+            <Card  sx={{ bgcolor: bgcolor || '', position: 'relative' }}>
+              {tipoDescuento !== "" && isOpen ? <ResumenDescuentosDetalle dataDetalle={dataDetalle} /> : ""}
+            </Card>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <Card sx={{ bgcolor: bgcolor || '', position: 'relative' }}>
+          {dataDescuentos.length > 0 && <GraficaDescuentos dataDescuentos={dataDescuentos} />}
+        </Card>
+      </Grid>
+      <Grid item xs={12}>
+        <Card sx={{ bgcolor: bgcolor || '', position: 'relative' }}>
+          {isOpen ?
+            <DescuentoDetalle tipoDescuento={tipoDescuento} setDataDetalle={setDataDetalle} /> : ""
+          }
+        </Card>
+      </Grid>
+
+    </>
   )
 }
 
