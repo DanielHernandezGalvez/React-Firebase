d27db874fa81f3394565ccd4adb6bd3d99716c63
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-05-18 09:10:33 -0600

    Update ResumentDescuentos.jsx

--- Sirsi/components/ResumentDescuentos.jsx
+++ Sirsi/components/ResumentDescuentos.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React from 'react';
 
-export default function ResumenDescuentos() {
+export default function ResumenDescuentos({ dataDescuentos }) {
+  const importeTotal = dataDescuentos.reduce((total, descuento) => total + descuento.ImporteTotal, 0);
+  const impTotal = importeTotal.toLocaleString("en-US", { minimumFractionDigits: 2 })
+  const cantidadDesc = dataDescuentos.reduce((total, descuento) => total + descuento.NumDescuentos, 0);
+  const subtotalGeneral = dataDescuentos.reduce((total, descuento) => total + descuento.Subtotal, 0);
+  const TotalGeneral = subtotalGeneral.toLocaleString("en-US", { minimumFractionDigits: 2 })
+  const descuentoGeneral = importeTotal - subtotalGeneral
+  const total = descuentoGeneral.toLocaleString("en-US", { minimumFractionDigits: 2 })
   return (
-    <div>Resumen</div>
-  )
+    <div className=" my-4 me-2 ms-2">
+      <div className="row">
+        <hr/>
+        <div className="col-8 ">
+          <b>Importe total:</b>
+        </div>
+        <div className="col-4 text-end">
+          <b>$ {impTotal}</b>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-8 ">
+          <b>No. Descuento:</b>
+        </div>
+        <div className="col-4 text-end">
+          <b>{cantidadDesc}</b>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-8 ">
+          <b>Descuento General:</b>
+        </div>
+        <div className="col-4 text-end ">
+          <b>$ {TotalGeneral}</b>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-8 ">
+          <b>Subtotal General:</b>
+        </div>
+        <div className="col-4 text-end">
+          <b>$ {total}</b>
+        </div>
+      </div>
+    </div>
+  );
 }
