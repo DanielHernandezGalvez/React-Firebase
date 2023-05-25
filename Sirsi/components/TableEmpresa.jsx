db8e82d33dac79ccfa029196c4c468e080162cab
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-05-25 09:05:48 -0600

    Update TableEmpresa.jsx

--- Sirsi/components/TableEmpresa.jsx
+++ Sirsi/components/TableEmpresa.jsx
@@ -1,23 +1,30 @@
-import React, { useEffect, useState } from "react";
-import FiltrosEmpresa from "./FiltrosEmpresa";
-import DataTable from "react-data-table-component";
+import React, { useEffect, useState } from 'react';
+import FiltrosEmpresa from './FiltrosEmpresa';
+import DataTable from 'react-data-table-component';
 
-export default function TablaEmpresas() {
-  const [check, setCheck] = useState("");
+import { useTheme } from '@mui/material/styles';
+import { Card, Grid, useMediaQuery } from '@mui/material';
+import { gridSpacing } from 'store/constant';
+
+export default function TablaEmpresas({ iconPrimary, primary, secondary, secondarySub, color, bgcolor }) {
+  const [check, setCheck] = useState('');
   const [dataLab, setDataLab] = useState([]);
   const [dataImg, setDataImg] = useState([]);
+
+  const theme = useTheme();
+  const matchDownXs = useMediaQuery(theme.breakpoints.down('sm'));
 
   const capitalize = (val) => {
     return val
       .toLowerCase()
       .trim()
-      .split(" ")
+      .split(' ')
       .map((v) => v[0].toUpperCase() + v.substr(1))
-      .join(" ");
+      .join(' ');
   };
 
   const selectEmpresas = async () => {
-    const url = process.env.RUTA_API + "/sirsi/web/BuscarEmpresas";
+    const url = process.env.RUTA_API + '/sirsi/web/BuscarEmpresas';
     // const url =  "http://192.168.0.14:8081/sirsi/web/BuscarEmpresas";
 
     try {
@@ -25,16 +32,16 @@ export default function TablaEmpresas() {
       let data = [];
       data = await response.json();
 
-      const select = document.getElementById("sucInputEmpresa");
-      select.innerHTML = "";
-      const option = document.createElement("option");
+      const select = document.getElementById('sucInputEmpresa');
+      select.innerHTML = '';
+      const option = document.createElement('option');
       // option.value = 0;
       // option.selected = true;
       // option.text = "";
       // select.appendChild(option);
 
       data.data.map((empresa) => {
-        const option = document.createElement("option");
+        const option = document.createElement('option');
         option.value = empresa.EmpresaId;
         option.text = capitalize(empresa.EmpresaNombre);
         select.appendChild(option);
@@ -45,16 +52,16 @@ export default function TablaEmpresas() {
   };
 
   const fechaActualEmpresa = () => {
-    const myDateInput = document.getElementById("fiInputEmpresa");
-    const ffInputBitacora = document.getElementById("ffInputEmpresa");
-    const fechaActual = new Date(new Date() - 6 * 60 * 60 * 1000).toISOString().split("T")[0];
+    const myDateInput = document.getElementById('fiInputEmpresa');
+    const ffInputBitacora = document.getElementById('ffInputEmpresa');
+    const fechaActual = new Date(new Date() - 6 * 60 * 60 * 1000).toISOString().split('T')[0];
     myDateInput.value = fechaActual;
     ffInputBitacora.value = fechaActual;
   };
 
   const diferenciaDias = () => {
-    const fiInput = document.getElementById("fiInputEmpresa");
-    const ffInput = document.getElementById("ffInputEmpresa");
+    const fiInput = document.getElementById('fiInputEmpresa');
+    const ffInput = document.getElementById('ffInputEmpresa');
 
     const fiDate = new Date(fiInput.value);
     const ffDate = new Date(ffInput.value);
@@ -63,9 +70,9 @@ export default function TablaEmpresas() {
     const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
     if (diffDays > 2) {
-      alert("La diferencia entre las fechas no puede ser mayor a 2 días.");
-      fiInput.value = "";
-      ffInput.value = "";
+      alert('La diferencia entre las fechas no puede ser mayor a 2 días.');
+      fiInput.value = '';
+      ffInput.value = '';
     }
   };
 
@@ -75,31 +82,30 @@ export default function TablaEmpresas() {
   }, []);
 
   useEffect(() => {
-    const fiInput = document.getElementById("fiInputEmpresa");
-    const ffInput = document.getElementById("ffInputEmpresa");
-    fiInput.addEventListener("change", diferenciaDias);
-    ffInput.addEventListener("change", diferenciaDias);
-  
+    const fiInput = document.getElementById('fiInputEmpresa');
+    const ffInput = document.getElementById('ffInputEmpresa');
+    fiInput.addEventListener('change', diferenciaDias);
+    ffInput.addEventListener('change', diferenciaDias);
+
     return () => {
-      fiInput.removeEventListener("change", diferenciaDias);
-      ffInput.removeEventListener("change", diferenciaDias);
+      fiInput.removeEventListener('change', diferenciaDias);
+      ffInput.removeEventListener('change', diferenciaDias);
     };
-  }, [])
+  }, []);
 
   const getEmpresa = async (event) => {
     event.preventDefault();
 
     try {
-      const fiDate = new Date(document.getElementById("fiInputEmpresa").value);
-      const ffDate = new Date(document.getElementById("ffInputEmpresa").value);
+      const fiDate = new Date(document.getElementById('fiInputEmpresa').value);
+      const ffDate = new Date(document.getElementById('ffInputEmpresa').value);
       const fi = Math.floor(fiDate.getTime() / 1000);
       const ff = Math.floor(ffDate.getTime() / 1000);
-      const emp = document.getElementById("sucInputEmpresa").value;
+      const emp = document.getElementById('sucInputEmpresa').value;
 
-      const laboratorios = document.getElementById("inputLaboratorio");
-      const imagenologia = document.getElementById("inputImagenologia");
-      var opciones = document.getElementById("opciones");
-
+      const laboratorios = document.getElementById('inputLaboratorio');
+      const imagenologia = document.getElementById('inputImagenologia');
+      var opciones = document.getElementById('opciones');
 
       const urlLabs = `${process.env.RUTA_API}/sirsi/web/BuscarResultadosLaboratorioEmpresa`;
       const urlImagen = `${process.env.RUTA_API}/sirsi/web/BuscarResultadosImagenologiaEmpresa`;
@@ -108,9 +114,9 @@ export default function TablaEmpresas() {
       //   alert("Debes ingresar el nombre de la empresa.");
       //   return false;
       // }
-    
+
       if (!laboratorios.checked && !imagenologia.checked) {
-        alert("Debes seleccionar Laboratorio o Imagenología.");
+        alert('Debes seleccionar Laboratorio o Imagenología.');
         return false;
       }
       // if (opciones.value === "") {
@@ -119,17 +125,17 @@ export default function TablaEmpresas() {
       // }
 
       let headers = new Headers();
-      headers.append("Content-Type", "application/json");
+      headers.append('Content-Type', 'application/json');
       var requestOptions = {
-        method: "POST",
+        method: 'POST',
         body: JSON.stringify({
-          Folios: "",
+          Folios: '',
           EmpresaId: parseInt(emp),
           FechaInicio: fi,
-          FechaFinal: ff,
+          FechaFinal: ff
         }),
-        redirect: "follow",
-        headers,
+        redirect: 'follow',
+        headers
       };
 
       let url;
@@ -139,7 +145,7 @@ export default function TablaEmpresas() {
         if (response.status === 200) {
           let data = await response.json();
           setDataLab(data.data);
-          setCheck("laboratorio");
+          setCheck('laboratorio');
         } else {
           alert(await response.text());
         }
@@ -149,7 +155,7 @@ export default function TablaEmpresas() {
         let data = await response.json();
         if (response.status === 200) {
           setDataImg(data.data);
-          setCheck("imagenologia");
+          setCheck('imagenologia');
         } else {
           alert(await response.text());
         }
@@ -161,59 +167,51 @@ export default function TablaEmpresas() {
 
   const columnsLaboratorio = [
     {
-      name: "Folio",
-      selector: "Folio",
-      sorteable: true,
+      name: 'Folio',
+      selector: 'Folio',
+      sorteable: true
     },
     {
-      name: "NombrePaciente",
-      selector: "NombrePaciente",
-      sorteable: true,
+      name: 'NombrePaciente',
+      selector: 'NombrePaciente',
+      sorteable: true
     },
     {
-      name: "Sexo",
-      selector: "Sexo",
-      sorteable: true,
+      name: 'Sexo',
+      selector: 'Sexo',
+      sorteable: true
     },
     {
-      name: "DescripcionEstudio",
-      selector: "DescripcionEstudio.String",
-      sorteable: true,
+      name: 'DescripcionEstudio',
+      selector: 'DescripcionEstudio.String',
+      sorteable: true
     },
     {
-      name: "Resultado",
-      selector: "Resultado.String",
-      sorteable: true,
-    },
+      name: 'Resultado',
+      selector: 'Resultado.String',
+      sorteable: true
+    }
   ];
 
   return (
     <>
-      <FiltrosEmpresa getEmpresa={getEmpresa} />
-      {console.log(check)}
-      {check === "laboratorio" && (
-        <DataTable
-          data={dataLab}
-          columns={columnsLaboratorio}
-          responsive='true'
-          pagination
-          fixedHeader
-          // fixedHeaderScrollHeight='600px'
-        //   scrollY='30vh'
-        //   scrollX='true'
-        />
+      <Grid item xs={12}>
+        <Card sx={{ bgcolor: bgcolor || '', position: 'relative' }}>
+          <FiltrosEmpresa getEmpresa={getEmpresa} />
+          {console.log(check)}
+          {check === 'laboratorio' && <DataTable data={dataLab} columns={columnsLaboratorio} responsive="true" pagination fixedHeader />}
+        </Card>
+      </Grid>
+
+      {check === 'imagenologia' && (
+        <Grid item xs={12}>
+          <Grid container spacing={gridSpacing}>
+            <Card sx={{ bgcolor: bgcolor || '', position: 'relative', paddingTop: '10px' }}>
+              <DataTable data={dataImg} columns={columnsLaboratorio} responsive="true" pagination fixedHeader />
+            </Card>
+          </Grid>
+        </Grid>
       )}
-      {check === "imagenologia" && (
-        <DataTable
-          data={dataImg}
-          columns={columnsLaboratorio}
-          responsive='true'
-          pagination
-          fixedHeader
-        //   fixedHeaderScrollHeight='60%'
-        />
-      )}
-      
     </>
   );
 }
