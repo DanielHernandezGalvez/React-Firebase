27b35ea1fc1baf32ff4ba70d7dbac8cd8c788a74
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-05-29 10:44:58 -0600

    Update pages.js

--- Sirsi/pages/pages.js
+++ Sirsi/pages/pages.js
@@ -2,7 +2,24 @@
 import { FormattedMessage } from 'react-intl';
 
 // assets
-import { IconKey, IconFolder, IconBook, IconClipboardCheck, IconTag, IconReceipt, IconTarget, IconBug, IconNotebook, IconBriefcase, IconFileText, IconBuilding, IconCheck, IconChartBar  } from '@tabler/icons';
+import {
+  IconKey,
+  IconFolder,
+  IconBook,
+  IconClipboardCheck,
+  IconTag,
+  IconReceipt,
+  IconTarget,
+  IconBug,
+  IconNotebook,
+  IconBriefcase,
+  IconFileText,
+  IconBuilding,
+  IconCheck,
+  IconChartBar,
+  IconLocation,
+  IconAlertOctagon  
+} from '@tabler/icons';
 
 // constant
 const icons = {
@@ -19,14 +36,16 @@ const icons = {
   IconTag,
   IconClipboardCheck,
   IconFolder,
-  IconBook
+  IconBook,
+  IconLocation,
+  IconAlertOctagon  
 };
 
 // ==============================|| EXTRA PAGES MENU ITEMS ||============================== //
 
 const pages = {
   id: 'pages',
-  title: "Sirsi Web",
+  title: 'Sirsi Web',
   icon: icons.IconNotebook,
   type: 'group',
   children: [
@@ -54,81 +73,95 @@ const pages = {
         }
       ]
     },
-   {
-    id: "Administracion",
-    title: <FormattedMessage id="Administracion" />,
-    type: 'collapse',
-    icon: icons.IconFileText,
-    children: [
-      {
-        id: 'Facturas',
-        title: <FormattedMessage id="Facturas" />,
-        type: 'item',
-        icon: icons.IconReceipt,
-        url: '/facturas',
-        target: false
-      }
-    ]
-   },
-   {
-    id: "Dirección",
-    title: <FormattedMessage id="Dirección" />,
-    type: 'collapse',
-    icon: icons.IconBuilding,
-    children: [
-      {
-        id: 'Descuentos',
-        title: <FormattedMessage id="Descuentos" />,
-        type: 'item',
-        icon: icons.IconTag,
-        url: '/direccion/descuentos',
-        target: false
-      }
-    ]
-   },
-   {
-    id: "Encuesta",
-    title: <FormattedMessage id="Encuesta" />,
-    type: 'collapse',
-    icon: icons.IconCheck,
-    children: [
-      {
-        id: 'ResultadosEncuesta',
-        title: <FormattedMessage id="ResultadosEncuesta" />,
-        type: 'item',
-        icon: icons.IconClipboardCheck,
-        url: '/encuesta/resultadosencuesta',
-        target: false
-      }
-    ]
-   },
-   {
-    id: "Productividad",
-    title: <FormattedMessage id="Productividad" />,
-    type: 'collapse',
-    icon: icons.IconChartBar,
-    children: [
-      {
-        id: 'Expedientes',
-        title: <FormattedMessage id="Expedientes" />,
-        type: 'item',
-        icon: icons.IconFolder,
-        url: '/productividad/expedientes',
-        target: false
-      },
-      {
-        id: 'Estudios',
-        title: <FormattedMessage id="Estudios" />,
-        type: 'item',
-        icon: icons.IconBook,
-        url: '/productividad/estudios',
-        target: false
-      }
-    ]
-   }
+    {
+      id: 'Administracion',
+      title: <FormattedMessage id="Administracion" />,
+      type: 'collapse',
+      icon: icons.IconFileText,
+      children: [
+        {
+          id: 'Facturas',
+          title: <FormattedMessage id="Facturas" />,
+          type: 'item',
+          icon: icons.IconReceipt,
+          url: '/facturas',
+          target: false
+        }
+      ]
+    },
+    {
+      id: 'Dirección',
+      title: <FormattedMessage id="Dirección" />,
+      type: 'collapse',
+      icon: icons.IconBuilding,
+      children: [
+        {
+          id: 'Descuentos',
+          title: <FormattedMessage id="Descuentos" />,
+          type: 'item',
+          icon: icons.IconTag,
+          url: '/direccion/descuentos',
+          target: false
+        }
+      ]
+    },
+    {
+      id: 'Encuesta',
+      title: <FormattedMessage id="Encuesta" />,
+      type: 'collapse',
+      icon: icons.IconCheck,
+      children: [
+        {
+          id: 'ResultadosEncuesta',
+          title: <FormattedMessage id="ResultadosEncuesta" />,
+          type: 'item',
+          icon: icons.IconClipboardCheck,
+          url: '/encuesta/resultadosencuesta',
+          target: false
+        }
+      ]
+    },
+    {
+      id: 'Productividad',
+      title: <FormattedMessage id="Productividad" />,
+      type: 'collapse',
+      icon: icons.IconChartBar,
+      children: [
+        {
+          id: 'Sucursal',
+          title: <FormattedMessage id="Sucursal" />,
+          type: 'collapse',
+          icon: icons.IconLocation,
+          children: [
+            {
+              id: 'Expedientes',
+              title: <FormattedMessage id="Expedientes" />,
+              type: 'item',
+              icon: icons.IconFolder,
+              url: '/productividad/expedientes',
+              target: false
+            },
+            {
+              id: 'Estudios',
+              title: <FormattedMessage id="Estudios" />,
+              type: 'item',
+              icon: icons.IconBook,
+              url: '/productividad/estudios',
+              target: false
+            },
+            {
+              id: 'Faltantes/Sobrantes',
+              title: <FormattedMessage id="Faltantes/Sobrantes" />,
+              type: 'item',
+              icon: icons.IconAlertOctagon  ,
+              url: '/productividad/faltantes/sobrantes',
+              target: false
+            }
+          ]
+        }
+      ]
+    }
   ]
 };
-
-
 
 export default pages;
