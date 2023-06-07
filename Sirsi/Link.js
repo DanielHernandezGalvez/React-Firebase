bcd3ea3d2f89cf4571b5cc1c2cfec571f500bc46
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-06-07 11:10:59 -0600

    Update Link.js

--- Sirsi/Link.js
+++ Sirsi/Link.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import * as React from 'react';
 
-// next imports imortacion no hecha
+// next imports
 import { useRouter } from 'next/router';
 import NextLinkComposed from 'NextLinkComposed';
 
