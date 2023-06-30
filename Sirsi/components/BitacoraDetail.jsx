2e3a3556f3976da2e4793c9ae4b3ce5f9096c2b1
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-06-30 15:49:08 -0600

    Update BitacoraDetail.jsx

--- Sirsi/components/BitacoraDetail.jsx
+++ Sirsi/components/BitacoraDetail.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import DataTable from "react-data-table-component";
 
 export default function BitacoraDetail({ cambioEncabezado, activeSection }) {
-  //  no Traer el data-table component
   const columnsEncabezados = [
     {
       name: "Campo",
