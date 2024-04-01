121685c083dc307ea3e16e9266a9987882bd13fe
unknown <hernandezgalvezalejandro@gmail.com>
2024-04-01 11:04:47 -0600

    listener

--- demo/demo.js
+++ demo/demo.js
@@ -1,2 +1,9 @@
 // http es una librería que viene integrada con node
-const http = require("http")
+const http = require("http");
+
+http.createServer((req, res) => {
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end("Holanga happy happy happy")
+
+}).listen(3001, "localhost")
