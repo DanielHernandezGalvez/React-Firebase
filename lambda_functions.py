6dcb226cb557abed671a245bd51e1d574ffe58d8
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2024-04-21 11:39:12 -0600

    Update lambda_functions.py

--- lambda_functions.py
+++ lambda_functions.py
@@ -1,3 +1,6 @@
+def add_expense(expenses, amount, category):
+    expenses.append({'amount': amount, 'category': category})
+    
 def print_expenses(expenses):
     for expense in expenses:
-        print(f'Amount: {expense}, Category: {expense}')
+        print(f'Amount: {expense["amount"]}, Category: {expense["category"]}')
