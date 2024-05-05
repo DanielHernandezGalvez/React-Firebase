d8a5432a0ab962a9100f6adb27233e42cfdc4537
Daniel Alejandro Hernández Gálvez <hernandezgalvezalejandro@gmail.com>
2024-05-05 13:32:09 -0600

    contenedor en rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import Contenedor from './elements/Contenedor'
 
 const App = () => {
   return (
-    <Contenedor>
     <h1>Hola mundo</h1>
-    </Contenedor>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -8,19 +8,21 @@ import GastosCategoria from "./components/GastosCategoria.jsx";
 import InisioSesion from "./components/InisioSesion.jsx";
 import ListaGastos from "./components/ListaGastos.jsx";
 import RegistroUsuarios from "./components/RegistroUsuarios.jsx";
+import Contenedor from "./elements/Contenedor.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+    <Contenedor>
       <Routes>
         <Route path="/iniciar-sesion" element={<InisioSesion />} />
         <Route path="/crear-cuenta" element={<RegistroUsuarios />} />
         <Route path="/categorias" element={<GastosCategoria />} />
         <Route path="/lista" element={<ListaGastos />} />
         <Route path="/editar/:id" element={<EditarGasto />} />
-        <Route path="/" element={ <App />} />
+        <Route path="/" element={<App />} />
       </Routes>
-     
+    </Contenedor>
     </BrowserRouter>
   </React.StrictMode>
 );
