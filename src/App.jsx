178f6b01a9db254e23da07eca1038aea326716e9
Daniel Alejandro Hernández Gálvez <hernandezgalvezalejandro@gmail.com>
2024-05-05 23:43:36 -0600

    seo y componente header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import {
+  Header,
+  Titulo,
+  ContenedorBotones,
+  ContenedorHeader,
+} from "./elements/Header";
+import { Helmet } from "react-helmet";
 
 const App = () => {
   return (
-    <h1>Hola mundo</h1>
-  )
-}
+    <>
+      <Helmet>
+        <title>Agregar Gasto</title>
+      </Helmet>
+      <Header>
+        <ContenedorHeader>
+          <Titulo>Agregar Gasto</Titulo>
+          <ContenedorBotones>
+            <button>Categorías</button>
+            <button>Lista de gastos</button>
+            <button></button>
+          </ContenedorBotones>
+        </ContenedorHeader>
+      </Header>
+    </>
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -9,20 +9,28 @@ import InisioSesion from "./components/InisioSesion.jsx";
 import ListaGastos from "./components/ListaGastos.jsx";
 import RegistroUsuarios from "./components/RegistroUsuarios.jsx";
 import Contenedor from "./elements/Contenedor.js";
+import { Helmet } from "react-helmet";
+import favicon from "./img/logo.png";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <Contenedor>
-      <Routes>
-        <Route path="/iniciar-sesion" element={<InisioSesion />} />
-        <Route path="/crear-cuenta" element={<RegistroUsuarios />} />
-        <Route path="/categorias" element={<GastosCategoria />} />
-        <Route path="/lista" element={<ListaGastos />} />
-        <Route path="/editar/:id" element={<EditarGasto />} />
-        <Route path="/" element={<App />} />
-      </Routes>
-    </Contenedor>
-    </BrowserRouter>
-  </React.StrictMode>
+  <>
+    <React.StrictMode>
+      <Helmet>
+        <link rel="shortcut icon" href={favicon} type="image/x-icon" />
+        <title>Mis finanzas</title>
+      </Helmet>
+      <BrowserRouter>
+        <Contenedor>
+          <Routes>
+            <Route path="/iniciar-sesion" element={<InisioSesion />} />
+            <Route path="/crear-cuenta" element={<RegistroUsuarios />} />
+            <Route path="/categorias" element={<GastosCategoria />} />
+            <Route path="/lista" element={<ListaGastos />} />
+            <Route path="/editar/:id" element={<EditarGasto />} />
+            <Route path="/" element={<App />} />
+          </Routes>
+        </Contenedor>
+      </BrowserRouter>
+    </React.StrictMode>
+  </>
 );
